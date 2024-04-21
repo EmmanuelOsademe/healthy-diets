d17d9d8d76ac34f6520205a13aaecdde28edd92e
Emmanuel Osademe <emma.osademe@gmail.com>
2024-04-21 14:20:00 +0100

    Create blogs

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,7 @@ import Navbar from "./components/Navbar";
 import CreateBlog from "./pages/CreateBlog";
 import HomePage from "./pages/Home";
 import ContactPage from "./pages/ContactPage";
-
-import SingleBlogPage from "./pages/SingleBlog";
+import SingleBlogPage from "./pages/SingleBlog2";
 
 import Footer from "./components/Footer";
 
@@ -32,7 +31,7 @@ const App = () => {
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/create-blog" element={<CreateBlog />} />
         <Route path="/blogs" element={<BlogPage />} />
-        <Route path="/single-blog" element={<SingleBlogPage />} />
+        <Route path="/single-blog/:slug" element={<SingleBlogPage />} />
         <Route path="/contact-us" element={<ContactPage />} />
       </Routes>
       <Footer />
--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 import { convertStringToDate } from "../utils";
 
 const BlogCard = ({ blog }) => {
@@ -13,9 +14,12 @@ const BlogCard = ({ blog }) => {
         <h3 className="text-2xl pt-6">{blog.title}</h3>
         <p className="mb-4 text-xs">{convertStringToDate(blog.createdAt)}</p>
         <p className="mb-4 pr-8 text-sm line-clamp-5">{blog.overview}</p>
-        <button className="bg-primaryOrange text-white w-32 h-10 rounded-full">
+        <Link
+          to={`/single-blog/${blog.slug}`}
+          className="bg-primaryOrange text-white w-32 h-10 rounded-full py-2 px-4"
+        >
           Read More
-        </button>
+        </Link>
       </div>
     </div>
   );
