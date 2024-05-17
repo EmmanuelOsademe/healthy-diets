cb43db95c4ac5ed1523c68f8fc87422e92ce0655
Ukwuezepascaline <ezikepascaline@gmail.com>
2024-05-17 05:51:49 -0700

    task

---
+++ src/pages/AccountPage.jsx
@@ -0,0 +1,34 @@
+import "../index.css";
+
+
+const AccountPage = () => {
+    return (
+
+        <div>
+            <h1>My Account</h1>
+
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>Jen Bell</h4>
+                    <p>jenbell@gmail.com</p>
+
+                    <button>Personal Information</button>
+
+                    <ul>
+                        <li>Appointments</li>
+                        <li>Menu Plan</li>
+                        <li>Settings</li>
+                        <li>Log Out</li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>Personal Information</h1>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default AccountPage;
